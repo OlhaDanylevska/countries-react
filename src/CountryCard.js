48ec8a0d47631e0cee39d006a5c8d04160349a5a
Olha Danylevska <odanilevskaya2@gmail.com>
2023-07-11 00:04:10 +0100

    search bar and dropdown for regions

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -1,56 +1,69 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import SearchBar from "./SearchBar"
+import FilterRegion from "./FilterRegion"
 
-const CountryCard = () => {
-    const [allCountries, setAllCountries] = useState([])
+const CountryCard = ({ Data }) => {
+    const [allCountries, setAllCountries] = useState(Data)
 
-    const fetchImage = () => {
-        fetch("https://restcountries.com/v3.1/all")
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.status)
-                } else {
-                    return response.json()
-                }
-            })
-            .then((data) => {
-                if (data) {
-                    console.log(data)
-                    setAllCountries(data)
-                }
-            })
-    }
 
-    useEffect(() => {
-        fetchImage()
-    }, [])
+    // const fetchImage = () => {
+    //     fetch("https://restcountries.com/v3.1/all")
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error(response.status)
+    //             } else {
+    //                 return response.json()
+    //             }
+    //         })
+    //         .then((data) => {
+    //             if (data) {
+    //                 setAllCountries(data)
+    //                 setFilteredCountries(data)
+    //             }
+    //         })
+    // }
+
+    // useEffect(() => {
+    //     fetchImage()
+    // }, []);
+
     return (
-        <div className="cards-container">
-            {allCountries.map(country => (
-                < div key={country.id} className="card" >
-                    <img src={country.flags.svg} className="flag-image" />
-                    <div className="text-holder" >
-                        <h3>{country.name.common}</h3>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th className="key">Population:</th>
-                                    <th className="value">{country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</th>
-                                </tr>
-                                <tr>
-                                    <th className="key">Region:</th>
-                                    <th className="value">{country.region}</th>
-                                </tr>
-                                <tr>
-                                    <th className="key">Capital:</th>
-                                    <th className="value">{country.capital}</th>
-                                </tr>
-                            </tbody>
+        <div className="page-holder">
+            <div className="header">
+                <SearchBar Data={Data} allCountries={allCountries} setAllCountries={setAllCountries} />
+                <FilterRegion Data={Data} setAllCountries={setAllCountries} />
+            </div>
 
-                        </table>
+            <div className="cards-container">
+
+                {allCountries.map(country => (
+                    < div key={country.id} className="card" >
+                        <img alt="flag" src={country.flags.svg} className="flag-image" />
+                        <div className="text-holder" >
+                            <h3>{country.name}</h3>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <th className="key">Population:</th>
+                                        <th className="value">{country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</th>
+                                    </tr>
+                                    <tr>
+                                        <th className="key">Region:</th>
+                                        <th className="value">{country.region}</th>
+                                    </tr>
+                                    <tr>
+                                        <th className="key">Capital:</th>
+                                        <th className="value">{country.capital}</th>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
+
     )
 }
 
---
+++ src/FilterRegion.js
@@ -0,0 +1,30 @@
+const FilterRegion = ({ Data, setAllCountries }) => {
+
+    const handleChange = (e) => {
+        if (e.target.value !== "") {
+            let regionFilteredCountry = Data.filter((country) => {
+                return country.region.toLowerCase() === e.target.value
+            })
+            setAllCountries(regionFilteredCountry)
+        } else {
+            setAllCountries(Data)
+        }
+    }
+
+    return (
+        <div>
+            <label for="rerion-select">Choose Region</label>
+            <select name="" id="region-select" onChange={handleChange}>
+                <option value="" selected>- Please Select Region -</option>
+                <option value="asia" >Asia</option>
+                <option value="africa">Africa</option>
+                <option value="europe">Europe</option>
+                <option value="americas">Americas</option>
+                <option value="oceania">Australia/Oceania</option>
+            </select>
+        </div>
+    )
+}
+
+
+export default FilterRegion
